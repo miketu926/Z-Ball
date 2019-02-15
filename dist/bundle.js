8a1ec5a3bcbdce60af4dd15411422045fe511186
Mike <miketu926@gmail.com>
2019-02-15 13:40:42 -0500

    Cleaned up code

--- dist/bundle.js
+++ dist/bundle.js
@@ -10704,7 +10704,7 @@ function play() {
       } else { // game.over === true
       requestAnimationFrame(gameOver);
       ctx.canvas.removeEventListener("click", clickHandler);
-      window.removeEventListener("keyup", clickHandler);
+      window.removeEventListener("keydown", clickHandler);
 
       if (game.score > top5[4]) {
         var topdiv = document.createElement("div");
@@ -11006,18 +11006,13 @@ class StartLane {
 /*!*********************!*\
   !*** ./src/util.js ***!
   \*********************/
-/*! exports provided: fetchScores, postScore, checkIfScoreIsgood, produceUl, generateHighScoreForm, createForm, clearForm */
+/*! exports provided: fetchScores, postScore */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "fetchScores", function() { return fetchScores; });
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "postScore", function() { return postScore; });
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "checkIfScoreIsgood", function() { return checkIfScoreIsgood; });
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "produceUl", function() { return produceUl; });
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "generateHighScoreForm", function() { return generateHighScoreForm; });
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "createForm", function() { return createForm; });
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "clearForm", function() { return clearForm; });
 /* harmony import */ var jquery__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js");
 /* harmony import */ var jquery__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(jquery__WEBPACK_IMPORTED_MODULE_0__);
 
@@ -11038,109 +11033,6 @@ const postScore = (name, score) => {
       "score": score
     })
   });
-};
-
-const checkIfScoreIsgood = (score, scores) => {
-  let arr = [];
-  Object.values(scores).forEach(el => {
-    arr.push(el.score);
-  });
-  arr = arr.sort(function (a, b) { return a - b }).reverse()
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] < score) {
-      return i;
-    } else if (i > 4) {
-      return -1;
-    }
-  }
-  return arr.length;
-}
-
-const produceUl = (scoresArray, rank, score) => {
-  const ul = document.getElementById('high-score-name');
-  const sul = document.getElementById('high-score-score');
-  let placed = false;
-
-  while (ul.firstChild) {
-    ul.removeChild(ul.firstChild);
-  }
-  while (sul.firstChild) {
-    sul.removeChild(sul.firstChild);
-  }
-
-  scoresArray.forEach((el, i) => {
-    let li = document.createElement("li");
-    let sli = document.createElement("li");
-
-    if (i === rank) {
-      createForm(ul, score);
-      let newli = document.createElement("li");
-      newli.appendChild(document.createTextNode(score));
-      sul.appendChild(newli);
-      placed = true;
-    }
-      li.appendChild(document.createTextNode(`${el.name}`));
-      ul.appendChild(li);
-      sli.appendChild(document.createTextNode(`${el.score}`));
-      sul.appendChild(sli);
-      if (i === scoresArray.length - 1 && !placed && rank) {
-        createForm(ul, score);
-        let newli = document.createElement("li");
-        newli.appendChild(document.createTextNode(score));
-        sul.appendChild(newli);
-    }
-  });
-};
-
-const generateHighScoreForm = (score) => {
-  fetchScores().then(scores => {
-    let rank = checkIfScoreIsgood(score, scores);
-    let arr = Object.values(scores);
-    if (rank > -1) {
-      produceUl(arr.sort(function (a, b) { return a.score - b.score; }).reverse().slice(0, 4), rank, score);
-      return true;
-    } else {
-      arr = Object.values(scores);
-      produceUl(arr.sort(function (a, b) { return a.score - b.score; }).reverse().slice(0, 5), false, score);
-      return false;
-    }
-  });
-};
-
-const createForm = (ul, score) => {
-  let my_form = document.createElement('FORM');
-  my_form.name = 'myForm';
-  my_form.method = 'POST';
-  my_form.onsubmit = (e) => {
-    e.preventDefault();
-    postScore(document.getElementById('name').value, score);
-  };
-
-  let inputEl = document.createElement('INPUT');
-  inputEl.type = 'TEXT';
-  inputEl.name = 'name';
-  inputEl.value = '';
-  inputEl.id = 'name';
-  inputEl.style = 'width: 120px;height:38px;font-size:36px;background-color:#c9cacc;border: 1px solid #c9cacc;';
-  inputEl.maxLength = "5";
-  my_form.appendChild(inputEl);
-
-
-  let formLi = document.createElement("li");
-  formLi.appendChild(my_form);
-  ul.appendChild(formLi);
-};
-
-const clearForm = () => {
-  const ul = document.getElementById('high-score-name');
-  const sul = document.getElementById('high-score-score');
-
-  while (ul.firstChild) {
-    ul.removeChild(ul.firstChild);
-  }
-  while (sul.firstChild) {
-    sul.removeChild(sul.firstChild);
-  }
 };
 
 /***/ })
